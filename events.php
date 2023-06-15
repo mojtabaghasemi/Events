3e772a1dba5bdf6ed4ebbc8c6828ba61e0c45a42
mojtaba <mojtabaghasemi75@yahoo.com>
2023-06-15 14:15:38 +0330

    update event and link in panel

--- events.php
+++ events.php
@@ -8,6 +8,7 @@
  * Author URI: https://sourcecity.ir/
  * License: GPL2
  */
+include "Payment.php";
 
 // Add a filter to modify the title of the blog
 add_filter( 'wp_title', 'my_plugin_modify_title', 10, 1 );
@@ -82,11 +83,20 @@ function event_settings_page() {
     );
 }
 add_action( 'admin_menu', 'event_settings_page' );
-function event_merchant_code_callback() {
-    $merchant_code = get_option( 'event_merchant_code', '' );
-    ?>
-    <input type="text" name="event_merchant_code" value="<?php echo esc_attr( $merchant_code ); ?>" />
-    <?php
+
+function event_merchant_id_callback() {
+    $value = get_option('event_merchant_id');
+    echo '<input type="text" name="event_merchant_id" value="' . esc_attr($value) . '">';
+}
+
+function event_terminal_id_callback() {
+    $value = get_option('event_terminal_id');
+    echo '<input type="text" name="event_terminal_id" value="' . esc_attr($value) . '">';
+}
+
+function event_transaction_key_callback() {
+    $value = get_option('event_transaction_key');
+    echo '<input type="text" name="event_transaction_key" value="' . esc_attr($value) . '">';
 }
 
 function event_enable_gateway_callback() {
@@ -102,32 +112,20 @@ function event_enable_gateway_callback() {
 function event_settings_callback() {
     ?>
     <div class="wrap">
-        <h1><?php _e( 'Event Settings', 'text-domain' ); ?></h1>
+        <h1><?php _e( 'تنظیمات رویدادها', 'text-domain' ); ?></h1>
         <form method="post" action="options.php">
             <?php settings_fields( 'event_settings_group' ); ?>
             <?php do_settings_sections( 'event_settings_page' ); ?>
             <?php submit_button( __( 'ذخیره تنظیمات', 'text-domain' ), 'primary', 'submit', true ); ?>
         </form>
     </div>
-    <script>
-        jQuery( document ).ready( function( $ ) {
-            $( '#event_disable_button' ).on( 'click', function( event ) {
-                event.preventDefault();
-                $( 'input[name="event_enable_gateway"]' ).prop( 'checked', false );
-            } );
-            $( '#event_enable_button' ).on( 'click', function( event ) {
-                event.preventDefault();
-                $( 'input[name="event_enable_gateway"]' ).prop( 'checked', true );
-            } );
-        } );
-    </script>
     <?php
 }
 
 function event_settings_init() {
     register_setting(
         'event_settings_group',
-        'event_merchant_code',
+        'event_transaction_key',
         array(
             'type' => 'string',
             'sanitize_callback' => 'sanitize_text_field',
@@ -143,19 +141,34 @@ function event_settings_init() {
         )
     );
 
+    // ثبت تنظیمات شماره پذیرنده
+    register_setting(
+        'event_settings_group',
+        'event_merchant_id',
+        array(
+            'type' => 'string',
+            'sanitize_callback' => 'sanitize_text_field',
+            'default' => ''
+        )
+    );
+
+// ثبت تنظیمات شماره ترمینال
+    register_setting(
+        'event_settings_group',
+        'event_terminal_id',
+        array(
+            'type' => 'string',
+            'sanitize_callback' => 'sanitize_text_field',
+            'default' => ''
+        )
+    );
+
     // اضافه کردن بخش‌های تنظیمات به صفحه تنظیمات
     add_settings_section(
         'event_settings_section',
-        __( 'تنظیمات رویدادها', 'text-domain' ),
+        __( 'تنظیمات حساب بانک ملی', 'text-domain' ),
         'event_settings_section_callback',
         'event_settings_page'
-    );
-    add_settings_field(
-        'event_merchant_code',
-        __( 'مرچنت کد', 'text-domain' ),
-        'event_merchant_code_callback',
-        'event_settings_page',
-        'event_settings_section'
     );
     add_settings_field(
         'event_enable_gateway',
@@ -164,5 +177,179 @@ function event_settings_init() {
         'event_settings_page',
         'event_settings_section'
     );
+
+    add_settings_field(
+        'merchant_id',
+        __( 'شماره پذیرنده', 'text-domain' ),
+        'event_merchant_id_callback',
+        'event_settings_page',
+        'event_settings_section'
+    );
+
+    add_settings_field(
+        'terminal_id',
+        __( 'شماره ترمینال', 'text-domain' ),
+        'event_terminal_id_callback',
+        'event_settings_page',
+        'event_settings_section'
+    );
+
+    add_settings_field(
+        'event_merchant_code',
+        __( 'کلید تراکنش', 'text-domain' ),
+        'event_transaction_key_callback',
+        'event_settings_page',
+        'event_settings_section'
+    );
+
 }
 add_action( 'admin_init', 'event_settings_init' );
+
+
+//function create_event_orders_table() {
+//    global $wpdb;
+//    $table_name = $wpdb->prefix . 'event_orders';
+//    $charset_collate = $wpdb->get_charset_collate();
+//
+//    $sql = "CREATE TABLE $table_name (
+//        id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+//        user_id BIGINT(20) UNSIGNED NOT NULL,
+//        event_id BIGINT(20) UNSIGNED NOT NULL,
+//        amount BIGINT(20) UNSIGNED NULL,
+//        status VARCHAR(50) NULL,
+//        order_date DATETIME NULL,
+//        PRIMARY KEY (id),
+//        FOREIGN KEY (event_id) REFERENCES {$wpdb->prefix}posts(ID),
+//        FOREIGN KEY (user_id) REFERENCES {$wpdb->prefix}users(ID)
+//    ) $charset_collate;";
+//
+//    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+//    dbDelta( $sql );
+//}
+//
+//add_action('init', 'create_event_orders_table');
+//
+//function create_event_payments_table() {
+//    global $wpdb;
+//    $table_name = $wpdb->prefix . 'event_payments';
+//    $charset_collate = $wpdb->get_charset_collate();
+//
+//    $sql = "CREATE TABLE $table_name (
+//        id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+//        user_id BIGINT(20) UNSIGNED NOT NULL,
+//        order_id BIGINT(20) UNSIGNED NOT NULL,
+//        amount BIGINT(20) UNSIGNED NULL,
+//        system_trace_no VARCHAR(50) NULL,
+//        retrival_ref_no VARCHAR(50) NULL,
+//        status VARCHAR(50) NULL,
+//        payment_date DATETIME NULL,
+//        PRIMARY KEY (id),
+//        FOREIGN KEY (order_id) REFERENCES {$wpdb->prefix}event_orders(ID),
+//        FOREIGN KEY (user_id) REFERENCES {$wpdb->prefix}users(ID)
+//    ) $charset_collate;";
+//
+//    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+//    dbDelta( $sql );
+//}
+//add_action('init', 'create_event_payments_table');
+
+add_action( 'admin_post_event_process_payment', 'event_process_payment' );
+add_action( 'admin_post_nopriv_event_process_payment', 'event_process_payment' );
+function event_process_payment() {
+    // Verify nonce
+    check_admin_referer( 'event_process_payment', 'event_process_payment_nonce' );
+
+    // Get post ID and payment amount
+    $post_id = intval( $_POST['post_id'] );
+    $amount = floatval( $_POST['amount'] );
+
+    $user_id = get_current_user_id();
+
+    if ( $user_id == 0 ) {
+        wp_redirect( '/my-account' );
+        exit;
+    } else {
+        global $wpdb;
+        $order_status = 'pending';
+        $table_name = $wpdb->prefix.'event_orders';
+
+        $query = $wpdb->prepare(
+            "SELECT * FROM $table_name WHERE user_id = %d AND status = %s AND event_id = %d",
+            $user_id,
+            $order_status,
+            $post_id
+        );
+        $result = $wpdb->get_row($query);
+        $order_id = $result->id;
+
+        if (!$result) {
+            $data = [
+                'user_id' => $user_id,
+                'event_id' => $post_id,
+                'amount' => $amount,
+                'status' => $order_status,
+                'order_date' => current_time('mysql')
+            ];
+            $result = $wpdb->insert($table_name, $data);
+            $order_id = $wpdb->insert_id;
+        }
+
+        if ($result) {
+            $gateway_enabled = get_option( 'event_enable_gateway' );
+            if ($gateway_enabled) {
+                $gateway_transaction_key = get_option('event_transaction_key');
+                $gateway_merchant_id = get_option('event_merchant_id');
+                $gateway_terminal_id = get_option('event_terminal_id');
+                $redirect_url =  plugin_dir_url( __FILE__ ) . 'single-event.php';
+
+                $payment_class = new Payment();
+                $payment_class
+                    ->transaction_key($gateway_transaction_key)
+                    ->merchant_id($gateway_merchant_id)
+                    ->terminal_id($gateway_terminal_id)
+                    ->redirect_url($redirect_url);
+
+                $payment_class->redirect_to_bank($amount, $order_id);
+            }else{
+                echo "درگاه پرداخت غیرفعال میباشد";
+            }
+        } else {
+            echo 'Error adding record.';
+        }
+    }
+}
+// Add new tab to My Account menu
+function order_events_endpoint() {
+    add_rewrite_endpoint( 'order-events', EP_ROOT | EP_PAGES );
+}
+add_action( 'init', 'order_events_endpoint' );
+
+function order_events_endpoint_link( $items ) {
+    $icon_color = get_option( 'primary_color' );
+    $items['order-events'] = ' رویدادهای من';
+    return $items;
+}
+add_filter( 'woocommerce_account_menu_items', 'order_events_endpoint_link' );
+
+function order_events_display() {
+    require 'Template/account-events.php';
+}
+add_action( 'woocommerce_account_order-events_endpoint', 'order_events_display' );
+
+function order_events_icon_color() {
+    $icon_color = get_option( 'primary_color' );
+    ?>
+    <style>
+        .order-events-icon:before {
+            color: #<?php echo $icon_color; ?>;
+        }
+    </style>
+    <?php
+}
+add_action( 'wp_head', 'order_events_icon_color' );
+
+add_action( 'wp_head', 'customstyle' ,99999);
+
+function customstyle(){
+    echo '<style>li.woocommerce-MyAccount-navigation-link--order-events a:before {content: "\f005";font-family: FontAwesome;color: #1597e5;margin-left: 2px}</style>';
+}
